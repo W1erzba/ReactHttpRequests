c0414529bf7ba4270ebe584c158bd057875d59a7
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-08 16:49:28 +0100

    data changed a little bit to be capable of data sending

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// Side note: API - Application Progarminng Interface
-
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -14,7 +13,9 @@ function App() {
 		setIsLoading(true);
 		setError(null);
 		try {
-			const response = await fetch('https://swapi.dev/api/films/');
+			const response = await fetch(
+				'https://react-http-4b003-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+			);
 			if (!response.ok) {
 				throw new Error('Something went wrong! 😶‍🌫️');
 			}
@@ -40,7 +41,11 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	let content = <p>Ups... Found no movies 😒</p>;
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
+
+	let content = <p>Found no movies 😵</p>;
 
 	if (movies.length > 0) {
 		content = <MoviesList movies={movies} />;
@@ -51,11 +56,14 @@ function App() {
 	}
 
 	if (isLoading) {
-		content = <p>Loading... please wait. 🫡</p>;
+		content = <p>Loading... ⌛</p>;
 	}
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
